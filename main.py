9b698b3c6b89dae2b8599f4103ee38448d4d3d5c
Ismael Mousa <ismaelramzimousa@gmail.com>
2024-05-19 00:04:16 +0300

    update some variables

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+from time import time
+
 import asyncio
-import time
+
 from tools import fetcher
 
-COLOR_GREEN = "\033[1;32m"
+
+GREEN = "\033[1;32m"
 
 if __name__ == "__main__":
-    start_time = time.time()
+    start_time = time()
     asyncio.run(fetcher.main())
-    print(COLOR_GREEN + "\nCompleted Successfully!\nTime Taken:", round(time.time() - start_time, 2), COLOR_GREEN + "s")
+    end_time = time()
+
+    print(GREEN + "\nCompleted Successfully!\nTime Taken:", round(end_time - start_time, 2), GREEN + "s")
