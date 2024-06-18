c592a12e39000cff566b79a57b740fdc962334ec
Ismael Mousa <ismaelramzimousa@gmail.com>
2024-06-18 21:28:17 +0300

    refactor and fix the infobox filed from csv files

--- sources/wiki.py
+++ sources/wiki.py
@@ -1,11 +1,10 @@
-import csv
 import json
 import os
 
+import asyncio
+import aiohttp
 from bs4 import BeautifulSoup
 from tqdm import tqdm
-import aiohttp
-import asyncio
 import pandas as pd
 
 from utils import get_config
@@ -17,16 +16,17 @@ NORMAL = "\033[0m"
 
 class Wiki:
     """
-        A class to scrape Wikipedia articles asynchronously and store them in various formats.
+    A class to scrape Wikipedia articles asynchronously
+    and store them in various formats.
 
-        Attributes:
-            __json_data (list): A list to store JSON data for scraped articles.
-            __base_dir (str): The base directory to save scraped data.
+    Attributes:
+        __json_data: a list to store JSON data for scraped articles.
+        __base_dir: the base directory to save scraped data.
     """
 
     def __init__(self):
-        self.__json_data = []
-        self.__base_dir = "./data/wiki/"
+        self.__base_dir: str = "./data/wiki/"
+        self.__json_data: list = []
 
         if not os.path.exists(self.__base_dir):
             os.makedirs(self.__base_dir)
@@ -34,18 +34,15 @@ class Wiki:
     async def __fetch(self,
                       session: aiohttp.ClientSession,
                       article_number: int,
-                      csv_writer: csv.writer,
                       progress_bar: tqdm) -> None:
         """
-            Fetches a Wikipedia article asynchronously.
+        Fetches a Wikipedia article asynchronously.
 
+        :param session: the aiohttp client session.
+        :param article_number: the article number.
+        :param progress_bar: the tqdm progress bar object to track progress.
 
-            :param session: The aiohttp client session.
-            :param article_number: The article number.
-            :param csv_writer: The CSV writer object to write data to a CSV file.
-            :param progress_bar: The tqdm progress bar object to track progress.
-
-            :return: None
+        :return: None
         """
         try:
             wiki_url = get_config().sources.wiki
@@ -54,16 +51,28 @@ class Wiki:
                 response.raise_for_status()
 
                 html = await response.text()
+
                 wiki = BeautifulSoup(markup=html, features="html.parser")
 
                 title = wiki.find("h1").text.strip()
+
                 url = str(response.url)
+
                 intro_summary = wiki.find("p").text.strip()
-                body = wiki.find(name="div", class_="mw-parser-output").text.strip()
-                sections = [section.text.strip() for section in wiki.find_all("h2")]
-                references = [ref["href"] for ref in wiki.select(".references a[href^='http'],"
-                                                                 " .references a[href^='https']")]
-                categories = [cat.text.strip() for cat in wiki.select(".mw-normal-catlinks ul li")]
+
+                body = wiki.find(name="div",
+                                 class_="mw-parser-output").text.strip()
+
+                sections = [section.text.strip() for section
+                            in wiki.find_all("h2")]
+
+                references = [ref["href"] for ref in
+                              wiki.select(".references a[href^='http'],"
+                                          " .references a[href^='https']")]
+
+                categories = [cat.text.strip() for cat
+                              in wiki.select(".mw-normal-catlinks ul li")]
+
                 infobox = {}
                 infobox_table = wiki.find(name="table", class_="infobox")
 
@@ -76,6 +85,7 @@ class Wiki:
                         if len(cells) == 2:
                             key = cells[0].text.strip()
                             value = cells[1].text.strip()
+
                             infobox[key] = value
 
                 data = {"ID": article_number,
@@ -90,88 +100,77 @@ class Wiki:
 
                 self.__json_data.append(data)
 
-                csv_writer.writerow([article_number,
-                                     title,
-                                     url,
-                                     intro_summary,
-                                     sections,
-                                     references,
-                                     categories,
-                                     infobox])
-
                 progress_bar.update(1)
 
-        except aiohttp.ClientError as e:
-            print(RED + f"Error fetching Wikipedia article {article_number}: {e}" + NORMAL)
+        except aiohttp.ClientError as err:
+            print(RED + f"Fetch Wiki Article {article_number}: {err}" + NORMAL)
 
     async def scrape(self, num_articles: int) -> None:
         """
-            Scrapes Wikipedia articles asynchronously and stores them in various formats.
+        Scrapes Wikipedia articles asynchronously
+        and stores them in various formats.
 
-
-            :param num_articles: The number of articles to fetch.
-
-            :return: None
+        :param num_articles: The number of articles to fetch.
+        :return: None
         """
         async with aiohttp.ClientSession() as session:
-            if not os.path.exists(self.__base_dir):
-                os.makedirs(self.__base_dir)
+            desc = BLUE + "Fetching Wikipedia Articles" + NORMAL
 
-            csv_filename = os.path.join(self.__base_dir, "articles.csv")
-            with open(csv_filename, "w", newline="", encoding="utf-8") as csv_file:
-                csv_writer = csv.writer(csv_file)
-                csv_writer.writerow(["ID",
-                                     "Title",
-                                     "URL",
-                                     "Body",
-                                     "Introduction/Summary",
-                                     "Sections/Headings",
-                                     "References",
-                                     "Categories",
-                                     "Infobox"])
+            progress_bar = tqdm(total=num_articles,
+                                desc=desc,
+                                unit="article")
 
-                progress_bar = tqdm(total=num_articles,
-                                    desc=BLUE + "Fetching Wikipedia Articles" + NORMAL,
-                                    unit="article")
+            tasks = []
+            for i in range(1, num_articles + 1):
+                tasks.append(self.__fetch(session, i, progress_bar))
+                if i % 30 == 0:
+                    await asyncio.sleep(1)
+                if i % 10 == 0:
+                    await asyncio.gather(*tasks)
+                    tasks = []
 
-                tasks = []
-                for i in range(1, num_articles + 1):
-                    tasks.append(self.__fetch(session, i, csv_writer, progress_bar))
-                    if i % 30 == 0:
-                        await asyncio.sleep(1)
-                    if i % 10 == 0:
-                        await asyncio.gather(*tasks)
-                        tasks = []
-                await asyncio.gather(*tasks)
+            await asyncio.gather(*tasks)
 
-                progress_bar.close()
+            progress_bar.close()
 
-            json_filename = os.path.join(self.__base_dir, "articles.json")
-            with open(json_filename, "w", encoding="utf-8") as json_file:
-                json.dump(self.__json_data, json_file, ensure_ascii=False, indent=4)
+            data_frame = pd.DataFrame(self.__json_data)
 
-            df = pd.DataFrame(self.__json_data)
-            excel_filename = os.path.join(self.__base_dir, "articles.xlsx")
-            df.to_excel(excel_filename, index=False)
+            csv_file = os.path.join(self.__base_dir, "articles.csv")
+            data_frame.to_csv(csv_file, index=False, encoding="utf-8")
 
-            parquet_filename = os.path.join(self.__base_dir, "articles.parquet")
-            df.to_parquet(parquet_filename, index=False)
+            json_file = os.path.join(self.__base_dir, "articles.json")
+
+            with open(file=json_file, mode="w", encoding="utf-8") as json_file:
+                json.dump(obj=self.__json_data,
+                          fp=json_file,
+                          ensure_ascii=False,
+                          indent=4)
+
+            excel_file = os.path.join(self.__base_dir, "articles.xlsx")
+            data_frame.to_excel(excel_file, index=False)
+
+            parquet_file = os.path.join(self.__base_dir, "articles.parquet")
+            data_frame.to_parquet(parquet_file, index=False)
 
 
 async def get_wiki() -> None:
     """
     The main function of the Wikipedia scraper.
+
     :return: None
     """
+    scraper = Wiki()
+
     while True:
         try:
-            num_articles = int(input("\n" + "Enter The Number of Articles to Fetch >> " + BLUE))
+            num_articles = int(input(BLUE + "\nHow Many >> " + NORMAL))
+
             if num_articles > 0:
                 break
-            else:
-                print(RED + "Please enter a positive integer number!" + NORMAL)
+
+            print(RED + "Please enter a positive integer number!" + NORMAL)
+
         except ValueError:
             print(RED + "Please enter a valid integer number!" + NORMAL)
 
-    scraper = Wiki()
     await scraper.scrape(num_articles=num_articles)
